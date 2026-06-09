b454e00aae55b6995e66717e39ea26ecfc23ebcf
napoknot21 <napoknot21@protonmail.com>
2026-06-09 15:57:55 +0200

    First commit renew aegis

---
+++ main.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+
+
+
+def main () -> int :
+    """
+    
+    """
+
+    return 0
+
+
+
+if __name__ == "__main__" :
+    """
+    
+    """
+    main()
---
+++ src/ui/app.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+import streamlit as st
+
+
+
+
+def app () :
+    """
+    
+    """
+    return None
